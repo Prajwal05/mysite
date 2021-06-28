27254873d7b85adaa0f5a1d4a8bdd0b4f0bcc042
Prajwal M U <62536194+Prajwal05@users.noreply.github.com>
2021-06-29 00:05:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-(function() {
-//===== Prealoder
-
-	window.onload = function () {
-		window.setTimeout(fadeout, 500);
-	}
-
-	function fadeout() {
-		document.querySelector('.preloader').style.opacity = '0';
-		document.querySelector('.preloader').style.display = 'none';
-	}
-})
+function clicking(){
+alert"Prajwal Nunni";
+}
 
 
 
